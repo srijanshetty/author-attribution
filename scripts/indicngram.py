62e4e79a056a6d142896f11268fe80ed9ce031fa
Srijan R Shetty <srijan.shetty@gmail.com>
2015-04-10 09:13:17 +0530

    Webpage

--- scripts/indicngram.py
+++
@@ -1,9 +0,0 @@
-from indicngramlib import *
-from sys import argv
-
-filename, input_file, output_file = argv
-
-ngram = indicNgram()
-ngram.bigram(input_file)
-ngram.printBifreq(output_file)
-# It will print the output to command line
